8d3d8ebb57594e386effd42c37eb05b0f7830b56
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-12-02 18:05:45 +0330

    getting predicted top items for a user

--- core/recommender/Recommender.py
+++ core/recommender/Recommender.py
@@ -1,19 +1,20 @@
 from core.model.BaseClasses import BasePredictor
 import numpy as np
-
+import pandas as pd
 
 class BaseRecommender():
     def __init__(self, model: BasePredictor, user_cols: list[str], item_cols: list[str]):
         self.model = model
         self.user_cols = user_cols
         self.item_cols = item_cols
+        self.predictions = dict()
     
     
-    def getTop(self, count: int = 10): 
-        preds = self.model.predictBatch(self.user_cols, self.item_cols).flatten()
-        topInds = np.argpartition(preds, count)[-count:]
+    def getTopForUser(self, user_id, count: int = 10): 
+        self.predictions[user_id] = self.model.predictBatch(pd.Series(user_id, index = range(len(self.item_cols))), self.item_cols).flatten()
+        topInds = np.argpartition(self.predictions, count)[-count:]
         
-        return [(self.user_cols.iloc[idx], self.item_cols.iloc[idx], preds[idx]) for idx in topInds]
+        return [[user_id, self.item_cols.iloc[idx], self.predictions[idx]] for idx in topInds]
     
     
     
